888a273b4a228daa09a3752069fad3ab75445b38
xjx00 <1504635269@qq.com>
2018-03-27 15:16:47 +0800

    Update views.py

--- rest/views.py
+++ rest/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import json
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
 # Create your views here.
+@csrf_exempt
 def hello(request):
     resp = {'Status': 200, 'Message': 'Hello AirCraft Control Apis.'}
-    return HttpResponse(json.dumps(resp), content_type="application/json")
+    return JsonResponse(resp, safe=False)
 
